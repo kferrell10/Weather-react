967a322e639814ad7d5aab22af0395828890cbaf
kferrell10 <74556235+kferrell10@users.noreply.github.com>
2021-03-01 17:02:20 -0800

    Update Weather.js
    confirm connection of local repository with new repository for Weather-react

--- my-app/src/Weather.js
+++ my-app/src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
     axios.get(apiUrl).then(handleResponse);
     return <Loader
         type="Puff"
-        color="#00BFFF"
+        color="#a0a0a0"
         height={100}
         width={100}
         timeout={3000} //3 secs
